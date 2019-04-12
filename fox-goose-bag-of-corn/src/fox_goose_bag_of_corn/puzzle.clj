4a37637d535201970d2093f28004c0903dccaf3a
Lewis Cawthorne <Lewis.Cawthorne@exlservice.com>
2019-04-12 12:22:23 -0400

    Solve fox-goose-and-bag-of-corn

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,18 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  (conj start-pos
+   [[:fox :corn][:goose :boat :you][]]
+   [[:fox :corn][:boat][:goose :you]]
+   [[:fox :corn][:boat :you][:goose]]
+   [[:you :fox :corn][:boat][:goose]]
+   [[:fox][:corn :boat :you][:goose]]
+   [[:fox][:boat][:corn :goose :you]]
+   [[:fox][:goose :boat :you][:corn]]
+   [[:goose :fox :you][:boat][:corn]]
+   [[:goose][:boat :fox :you][:corn]]
+   [[:goose][:boat][:corn :fox :you]]
+   [[:goose][:boat :you][:corn :fox]]
+   [[:goose :you][:boat][:corn :fox]]
+   [[][:boat :goose :you][:corn :fox]]
+   [[][:boat][:goose :you :corn :fox]]))
